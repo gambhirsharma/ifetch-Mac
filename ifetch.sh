8fe56fbd070ca7db713e695c7dcf2df6e4c42abd
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-03-05 02:04:04 +0530

    Update ifetch.sh for MacOS

--- ifetch.sh
+++ ifetch.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-. /etc/os-release
+# Parsing SystemVersion.plist for macOS information
+PRODUCT_NAME=$(defaults read /System/Library/CoreServices/SystemVersion.plist ProductName)
+PRODUCT_VERSION=$(defaults read /System/Library/CoreServices/SystemVersion.plist ProductVersion)
+ID=$(echo "$PRODUCT_NAME" | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]')
 
 BLACK='[0;30m'
 RED='[0;31m'
@@ -15,18 +18,18 @@ RESET='[0m'
 PID=$(($RANDOM % 808 + 1))
 SPRITE=$(cat "$(dirname $0)/colorscripts/${PID}" | sed -e 's/$/[0m/g')
 
-HOST_NAME=$(cat /sys/devices/virtual/dmi/id/product_name) 
-HOST_VERSION=$(cat /sys/devices/virtual/dmi/id/product_version)
-TOTAL_MEMORY=$(free -m | awk 'NR==2 {print $2}')
-USAGE_MEMORY=$(free -m | awk 'NR==2 {print $3}')
-COLOR="$BLACK $RED $GREEN $YELLOW $BLUE $PURPLE $CYAN $WHITE $RESET"
+HOST_NAME=$(hostname)
+KERNEL_VERSION=$(uname -r)
+MEMORY_STATS=$(sysctl hw.memsize | awk '{print $2}')
+TOTAL_MEMORY=$((MEMORY_STATS / 1024 / 1024))
+USAGE_MEMORY=$(top -l 1 | awk '/PhysMem/ {print $2}' | tr -d 'MB')
 
 out=$(
 cat << EOF
 # ${PURPLE}$USER@$HOSTNAME${RESET}
 ├─ ${BLUE}os${RESET}: $ID
-├─ ${BLUE}host${RESET}: $HOST_NAME $HOST_VERSION
-├─ ${BLUE}kernel${RESET}: $(uname -r)
+├─ ${BLUE}host${RESET}: $HOST_NAME $PRODUCT_VERSION
+├─ ${BLUE}kernel${RESET}: $KERNEL_VERSION
 ├─ ${BLUE}memory${RESET}: ${USAGE_MEMORY}/${TOTAL_MEMORY} Mib
 └─ $COLOR
 EOF
